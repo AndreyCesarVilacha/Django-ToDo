55afa9c5cc5c9e7693acd39c8ec2a8d63a324fcd
Andrey Cesar <andreycesarrodrigues@hotmail.com>
2021-07-21 16:57:02 -0300

    finalizado

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import LisdtAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.generics import CreateAPIView
 from rest_framework.generics import DestroyAPIView
 from rest_framework.generics import UpdateAPIView
